5bf9fe580733ebf2ab4f99a9b16453a2d8090f2c
aaronjiang <1327994105@qq.com>
2018-11-15 23:02:18 +0800

    Initial commit

---
+++ src/main/java/pro/onlyou/wechat/officalaccount/OfficalAccountApplication.java
@@ -0,0 +1,12 @@
+package pro.onlyou.wechat.officalaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficalAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficalAccountApplication.class, args);
+    }
+}
